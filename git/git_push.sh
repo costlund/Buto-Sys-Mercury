de7797f9a3ebb0abfb3e8ab82c0c8d23800c11e5
Claes Ö <>
2020-11-24 10:56:42 +0100

    1.26.1

---
+++ git/git_push.sh
@@ -0,0 +1,13 @@
+
+
+cd ../../../sys
+find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo "" && echo "[sys]" && echo {} && cd {} && git push && echo)' \;
+cd ../sys/mercury/git
+
+cd ../../../theme
+find . -maxdepth 2 -mindepth 2 -type d -exec sh -c '(echo "" && echo "[theme]" && echo {} && cd {} && git push && echo)' \;
+cd ../sys/mercury/git
+
+cd ../../../plugin
+find . -maxdepth 2 -mindepth 2 -type d -exec sh -c '(echo "" && echo "[plugin]" && echo {} && cd {} && git push && echo)' \;
+cd ../sys/mercury/git
