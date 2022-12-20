2286b9dcb60fbccdbe1d82847c92644392b0ecd1
Claes Östlund <>
2022-12-20 20:02:23 +0100

    2.75.0

--- lib/wfElement.class.php
+++ lib/wfElement.class.php
@@ -1,38 +1,6 @@
 <?php
 class wfElement {
-    
-  public static function isSecurezzz($element){
-
-    /**
-     * If settings/secure is set we check if user has role.
-     */
-    if(wfArray::isKey($element, 'settings/secure')){
-      $secure = wfArray::get($element, 'settings/secure');
-      $role = wfUser::getRole();
-//      wfHelp::yml_dump($secure);
-//      wfHelp::yml_dump($role);
-      $hide = true;
-      foreach ($secure as $key => $value) {
-        if(!$value){
-          if(in_array($key, $role)){
-            $hide = false; break;
-          }
-        }
-      }
-      if($hide){
-        return true;
-      }else{
-        return false;
-      }
-    }else{
-      return false;
-    }
-    
-    
+  public static function get($dir, $function){
+    return new PluginWfYml($dir.'/element/'.$function.'.yml');
   }
-
-    
-    
 }
-
-?>
